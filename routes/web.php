ca4fdf050338d03c31203bf0845a59f420b480f3
huytb <huytb@dore.vn>
2018-03-22 15:36:07 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('wordpress', 'MyController@Getmetadata');
+Route::get('wordpress', ['as'=>'wordpress' ,'uses'=> 'MyController@Getmetadata']);
 
 
