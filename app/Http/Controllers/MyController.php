9f207b4e9e61490ce734c33dbc4477936001a856
huytb <huytb@dore.vn>
2018-03-22 15:28:34 +0700

    first commit

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -8,26 +8,26 @@ use App\Wordpress;
 class MyController extends Controller
 {
 	public function Getmetadata(){
-
-		$data = Data::get();
-		foreach ($data as $key => $value) {
-			$id = $value ->id;
-			$title = $value->title;
-			$content = $value ->content;
-			$dis = $value ->description;
-			$photo = $value ->photo;
-			$status = $value ->status;
-			$category = $value ->category;
-		}
-
 		$endpoint = "http://localhost/blog-dulich/xmlrpc.php";
+		$userwp = 'thaihuy';
+		$password = 'crossgame54321';
 
-		$photo = 'C:\Users\DORE\Desktop\caocao.jpg';
+		$Wordpress = new Wordpress($endpoint, $userwp, $password);
 
-		$Wordpress = new Wordpress($endpoint, 'thaihuy', 'crossgame54321');
+		$Data = Data::where('status',1)->get();
+		foreach ($Data as $key => $data) {
+			$id = $data->id;
+			$title = $data->title;
+			$content = $data->content;
+			$photo = $data->photo;
+			$category = $data->category;
+			$keyword = $data->tags;
 
-		$post = $Wordpress->submit($title,$content,$category,$photo,'1');
+			$postID = $Wordpress->submit($title,$content,$category,$photo,$keyword);
 
-		$uploadfile = $Wordpress->uploadMedia($title,$photo);
+			echo 'Post bài thành công với postid = ' . $postID. '<br>';
+
+			if($postID) Data::where('id',$id)->update(['status' => 2]);
+		}		
 	}
 }
